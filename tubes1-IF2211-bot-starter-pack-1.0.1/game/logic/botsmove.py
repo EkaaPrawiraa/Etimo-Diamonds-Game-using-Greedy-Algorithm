7d97a3c03f915efcbfd553dccc5f16b907764028
OrdinaryAzazel <19622052@mahasiswa.itb.ac.id>
2024-02-24 19:39:44 +0700

    movebots

---
+++ tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/botsmove.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from game.logic.base import BaseLogic
+from game.models import Board,GameObject,Position
+from typing import Optional
+from ..util import get_direction
+
+
+
+##algortima nextmove
+class BotsMove(BaseLogic):
+  def __init__(self):
+    #Intialize attribute necessary
+        self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        self.goal_position: Optional[Position] = None
+        self.current_direction = 0
+        #sisanya apa
+  def next_move(self, board_bot: GameObject, board: Board):
+        props = board_bot.properties
+        # Analyze new state
+        if props.diamonds == 5:
+            # Move to base
+            base = board_bot.properties.base
+            self.goal_position = base
+        else:
+            # Just roam around
+            self.goal_position = None
+
+        current_position = board_bot.position
+        if self.goal_position:
+            
