3aa0632a6e8a29cc6d7b47498e36aa53845dcda1
OrdinaryAzazel <19622052@mahasiswa.itb.ac.id>
2024-02-25 02:14:52 +0700

    update

--- tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/botsmove.py
+++ tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/botsmove.py
@@ -4,7 +4,13 @@ from game.models import Board,GameObject,Position
 from typing import Optional
 from ..util import get_direction
 import random
+import time
 
+# Tubes Stima 1 Point :
+# 1. Utamain diamond terdekat dan diamond merah kalo bisa
+# 2. Kalo ada base terdekat dan jauh dari diamond dan inventory tidak kosong balik ke base
+# 3. menjauh dari pemain lawan atau ambil inventorynya
+# 4. optimalin teleport dan red button
 
 
 ##algortima nextmove
@@ -53,35 +59,40 @@ class BotsMove(BaseLogic):
                 totalpointblock=0
                 listrangediamond=[]
                 for i in range(block_start_row,block_end_row):
-                    #  print("masuk1\n")
                      for j in range(block_start_col,block_end_col):
-                        #   print("masuk2\n")
                           for diamond in diamond_objects:
-                            #    print(f"total :\n",diamond.properties.points)
                                if (diamond.position.x==i ) and (diamond.position.y==j):
-                                    # print("Masuk3\n")
                                     totalpointblock+=diamond.properties.points
-                                    print(f"total :\n",diamond.properties.points)
+                                    # print(f"total :\n",diamond.properties.points)
                                     listrangediamond.append((abs(abs(diamond.position.x - current_position.x) + abs(diamond.position.y - current_position.y)), diamond.position))
 
                 
+                # print(f"total :\n",totalpointblock)
                 if totalpointblock==0:
-                     #pindah block
+                    #pindah block
                     #kode sementara
-                    print("Random\n")
-                    delta = self.directions[self.current_direction]
-                    delta_x = delta[0]
-                    delta_y = delta[1]
-                    if random.random() > 0.6:
-                        self.current_direction = (self.current_direction + 1) % len(
-                            self.directions
-                        )
+                    start_time = time.time()
+                    for diamond in diamond_objects:
+                            listrangediamond.append((abs(abs(diamond.position.x - current_position.x) + abs(diamond.position.y - current_position.y)), diamond.position))
+                    
+                    sorted_listrangediamond = sorted(listrangediamond, key=lambda x: x[0])
+                    _,self.goal_position=sorted_listrangediamond[0]
+                    end_time = time.time()
+                    print(f"Time : {end_time-start_time} sec\n")
+
+                    
+                    delta_x, delta_y = get_direction(
+                        current_position.x,
+                        current_position.y,
+                        self.goal_position.x,
+                        self.goal_position.y,
+                    )
                 else:
                      #cri terdekat
                     sorted_listrangediamond = sorted(listrangediamond, key=lambda x: x[0])
                     _,self.goal_position=sorted_listrangediamond[0]
 
-
+                    # print(f"Position goals: ({self.goal_position.x}, {self.goal_position.y})")
                     delta_x, delta_y = get_direction(
                         current_position.x,
                         current_position.y,
@@ -89,3 +100,28 @@ class BotsMove(BaseLogic):
                         self.goal_position.y,
                     )
             return delta_x,delta_y
+    
+    # def totalpointblock(self,start_position,diamond_objects):
+    #     whichBlock_x = 15//5
+    #     whichBlock_y=15//5
+    #     totaleveryblock=[]
+    #     for i in range(3):
+    #          for j in range(3):
+    #             if i != start_position.x and j !=start_position.y:
+    #                 block_start_row = whichBlock_x * 5
+    #                 block_end_row = (whichBlock_x + 1) * 5
+    #                 block_start_col = whichBlock_y * 5
+    #                 block_end_col = (whichBlock_y + 1) * 5
+    #                 totalpointblock=0
+    #                 listrangediamond=[]
+    #                 for i in range(block_start_row,block_end_row):
+    #                         for j in range(block_start_col,block_end_col):
+    #                             for diamond in diamond_objects:
+    #                                 if (diamond.position.x==i ) and (diamond.position.y==j):
+    #                                     totalpointblock+=diamond.properties.points
+    #                                     print(f"total :\n",diamond.properties.points)
+    #                                     listrangediamond.append((abs(abs(diamond.position.x - start_position.x) + abs(diamond.position.y - start_position.y)), diamond.position))
+    #                 sorted_listrangediamond = sorted(listrangediamond, key=lambda x: x[0])
+    #                 _,self.goal_position=sorted_listrangediamond[0]
+            
+         
