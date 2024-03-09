ddc5beff48285e192ea97b6124b57d4a1cc7b951
OrdinaryAzazel <19622052@mahasiswa.itb.ac.id>
2024-03-09 22:10:27 +0700

    Update main.py

--- src/tubes1-IF2211-bot-starter-pack-1.0.1/main.py
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/main.py
@@ -14,8 +14,8 @@ from game.logic.kangTackle import kangTackle
 from game.logic.pAdu import pAdu
 
 init()
-# BASE_URL = "http://localhost:3000/api"
-BASE_URL = "http://20.243.68.103:8082/api/"  ##punya edbert
+BASE_URL = "http://localhost:3000/api"
+# BASE_URL = "http://20.243.68.103:8082/api/"  ##punya edbert
 DEFAULT_BOARD_ID = 10
 CONTROLLERS = {
     "BotsMove": BotsMove,"RandomLogic":RandomLogic,"kangTackle":kangTackle,"pAdu":pAdu,
